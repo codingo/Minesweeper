17fcc1653458f95a44a085e84b33f229202ed6e3
Michael <886344+codingo@users.noreply.github.com>
2018-01-30 21:04:21 +1000

    Create update_sources.py

---
+++ lib/update_sources.py
@@ -0,0 +1,23 @@
+from download_file import download_file
+
+# to be updated later for the processing of additional lists
+SOURCEFILE = 'https://github.com/ZeroDot1/CoinBlockerLists/blob/master/list.txt'
+DESTINATION = 'sources.txt'
+
+def print_banner():                                          
+    print("______ _                                       ")
+    print("|     |_|___ ___ ___ _ _ _ ___ ___ ___ ___ ___ ")
+    print("| | | | |   | -_|_ -| | | | -_| -_| . | -_|  _|")
+    print("|_|_|_|_|_|_|___|___|_____|___|___|  _|___|_|  ")
+    print(" Crypto Mining Detecter BApp      |_|")
+    print(' Authored by @codingo_')
+
+def update_sources():
+    print('[-] Downloading and writing sources.txt file...')
+    download_file(SOURCEFILE, DESTINATION)
+
+def main():
+    print_banner()
+    
+
+main()
