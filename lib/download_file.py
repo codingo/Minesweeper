33a302cbc85a77d27d2697856e4de178387dc4a1
Michael <886344+codingo@users.noreply.github.com>
2018-01-31 17:31:49 +1000

    Updated print statements to fit applications standard

--- lib/download_file.py
+++ lib/download_file.py
@@ -7,9 +7,9 @@ def download_file(source, destination):
     try:
         urllib.request.urlretrieve(source, destination)
     except IOError:
-        print('IOError: An error occured when trying to write a downloaded file. \
+        print('[!] IOError: An error occured when trying to write a downloaded file. \
                 Do you already have it open?')
     except urllib.error.URLError:
-        print('URLError: An error occured trying to download the file. \
+        print('[!] URLError: An error occured trying to download the file. \
                 Please check the source and try again.')
 
