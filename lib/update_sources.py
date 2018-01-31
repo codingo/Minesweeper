65399314594a6b9f4e7ab0f5e3669e655636f01b
Michael <886344+codingo@users.noreply.github.com>
2018-01-31 10:58:46 +1000

    PEP8 refactoring

--- lib/update_sources.py
+++ lib/update_sources.py
@@ -3,11 +3,12 @@
 from download_file import download_file
 
 # to be updated later for the processing of additional lists
-SOURCEFILE = 'https://raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/list.txt'
+SOURCEFILE = 'https://raw.githubusercontent.com/ZeroDot1/ \
+                CoinBlockerLists/master/list.txt'
 DESTINATION = './sources.txt'
 
 
-def print_banner():                                          
+def print_banner():
     print("______ _                                       ")
     print("|     |_|___ ___ ___ _ _ _ ___ ___ ___ ___ ___ ")
     print("| | | | |   | -_|_ -| | | | -_| -_| . | -_|  _|")
@@ -21,9 +22,10 @@ def update_sources(source, destination):
     download_file(source, destination)
     print('[+] Successfully downloaded and wrote local sources')
 
+
 def main():
     print_banner()
     update_sources(SOURCEFILE, DESTINATION)
-    
+
 
 main()
